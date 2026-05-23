2c6ccac3e6964f72ecb89e5652b9e2c58b80d882
ofmiq <goncarm354@gmail.com>
2026-05-23 22:21:26 +0200

    feat(simulator): add SimConfig struct and default constants

---
+++ simulator/args.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATOR_ARGS_H
+#define SIMULATOR_ARGS_H
+
+#define DEFAULT_TICKERS 5
+#define DEFAULT_RATE 100
+#define DEFAULT_BURST 0.5
+#define DEFAULT_CHAOS 0.3
+#define DEFAULT_DURATION 60
+
+#define ARGS_OK 0
+#define ARGS_ERROR 1
+
+typedef struct
+{
+     int    num_tickers;
+     int    rate;
+     double burst;
+     double chaos;
+     int    duration;
+} SimConfig;
+
+#endif
