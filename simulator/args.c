e2d596a2293cfec57a8d0ef2238ee2ecbccf36b3
ofmiq <goncarm354@gmail.com>
2026-05-23 23:31:30 +0200

    feat(simulator): implement flag parsing in parse_args

--- simulator/args.c
+++ simulator/args.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "args.h"
 
 int parse_args(int argc, char *argv[], SimConfig *config)
@@ -10,5 +13,29 @@ int parse_args(int argc, char *argv[], SimConfig *config)
      config->chaos       = DEFAULT_CHAOS;
      config->duration    = DEFAULT_DURATION;
 
-     /* implement flag handling */
+     while((opt = getopt(argc, argv, "t:r:b:c:d:")) != -1)
+     {
+          switch(opt)
+          {
+          case 't':
+               config->num_tickers = atoi(optarg);
+               break;
+          case 'r':
+               config->rate = atoi(optarg);
+               break;
+          case 'b':
+               config->burst = atof(optarg);
+               break;
+          case 'c':
+               config->chaos = atof(optarg);
+               break;
+          case 'd':
+               config->duration = atoi(optarg);
+               break;
+          default:
+               return ARGS_ERROR;
+          }
+     }
+
+     return ARGS_OK;
 }
