a7b728dcb979d5c493d39620da2d62924036b805
ofmiq <goncarm354@gmail.com>
2026-05-30 21:48:33 +0200

    feat(simulator): add random burst to generator

--- simulator/generator.c
+++ simulator/generator.c
@@ -1,11 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "generator.h"
 
 static const char *TICKER_NAMES[] = {"AAPL", "GOOG", "MSFT", "AMZN", "TSLA",
                                      "META", "NVDA", "NFLX", "AMD",  "INTC"};
+
+static void sleep_us(long long us)
+{
+     struct timespec ts;
+
+     if(us <= 0)
+     {
+          return;
+     }
+
+     ts.tv_sec  = us / 1000000;
+     ts.tv_nsec = (us % 1000000) * 1000;
+
+     nanosleep(&ts, NULL);
+}
+
+static long long compute_pause(SimState *state)
+{
+     long long base_pause;
+     double    roll;
+
+     base_pause = 1000000 / state->config->rate;
+
+     roll = (double)rand() / RAND_MAX;
+
+     if(roll < state->config->burst)
+     {
+          return base_pause / 100;
+     }
+
+     return base_pause * (1 + (rand() % 3));
+}
 
 static void generate_one(SimState *state)
 {
@@ -57,11 +90,18 @@ void state_destroy(SimState *state)
 
 void run_generator(SimState *state) 
 {
+     long long pause_us;
      long long max_time;
+
      max_time = (long long)state->config->duration * 1000000;
 
      while(state->config->duration == 0 || state->sim_time < max_time)
      {
           generate_one(state);
+
+          pause_us         = compute_pause(state);
+          state->sim_time += pause_us;
+
+          sleep_us(pause_us);
      }
 }
